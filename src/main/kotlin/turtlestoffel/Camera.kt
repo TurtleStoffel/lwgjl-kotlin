a45553a27c7428dde1925828fb069e4084e01028
TurtleStoffel <me@turtlestoffel.com>
2023-10-26 20:50:22 +0200

    Improve camera movement

--- src/main/kotlin/turtlestoffel/Camera.kt
+++ src/main/kotlin/turtlestoffel/Camera.kt
@@ -7,25 +7,27 @@ import org.lwjgl.glfw.GLFW.*
 import java.nio.FloatBuffer
 import kotlin.math.PI
 
-const val CAMERA_SPEED = 0.3f
+const val CAMERA_SPEED = 0.1f
 
 class Camera {
-    private val position = Vector3f(0.0f, -3.0f, 1.0f)
+    private val position = Vector3f(0.0f, -5.0f, 1.0f)
+    private val centerOffset = Vector3f(0.0f, 5.0f, -1.0f)
+
     private val direction = Vector3f(0.0f, 0.0f, 0.0f)
 
     fun handleKeyInput(key: Int, action: Int) {
         if (key == GLFW_KEY_W && action == GLFW_PRESS) {
-            direction.y += CAMERA_SPEED
+            direction.z += CAMERA_SPEED
         }
         if (key == GLFW_KEY_W && action == GLFW_RELEASE) {
-            direction.y -= CAMERA_SPEED
+            direction.z -= CAMERA_SPEED
         }
 
         if (key == GLFW_KEY_S && action == GLFW_PRESS) {
-            direction.y -= CAMERA_SPEED
+            direction.z -= CAMERA_SPEED
         }
         if (key == GLFW_KEY_S && action == GLFW_RELEASE) {
-            direction.y += CAMERA_SPEED
+            direction.z += CAMERA_SPEED
         }
 
         if (key == GLFW_KEY_A && action == GLFW_PRESS) {
@@ -49,10 +51,12 @@ class Camera {
 
     fun getViewProjectionMatrix(): FloatBuffer {
         val cameraBuffer = BufferUtils.createFloatBuffer(16)
+        val center = Vector3f(position).add(centerOffset)
         Matrix4f()
             .perspective((PI /2).toFloat(), Engine.WINDOW_SIZE.first.toFloat()/ Engine.WINDOW_SIZE.second, 0.01f, 100.0f)
-            .lookAt(position.x, position.y, position.z,
-                0.0f, 0.0f, 0.0f,
+            .lookAt(
+                position.x, position.y, position.z,
+                center.x, center.y, center.z,
                 0.0f, 0.0f, 1.0f
             ).get(cameraBuffer)
 
