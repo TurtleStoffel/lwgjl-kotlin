94498bb6c1faf41cadf7b1ddb0746da368d60485
TurtleStoffel <me@turtlestoffel.com>
2023-10-29 11:32:11 +0100

    Apply lighting to world normal iso object normal

--- src/main/resources/vertex.glsl
+++ src/main/resources/vertex.glsl
@@ -12,7 +12,8 @@ const vec3 lightDirection = normalize(vec3(1.0f, -0.5f, 1.0f));
 const float ambientLighting = 0.35;
 
 void main() {
+    vec4 worldNormal = modelMatrix * vec4(normal, 0);
     gl_Position = viewProjectionMatrix * modelMatrix * vec4(pos, 1);
-    float brightness = max(dot(lightDirection.xyz, normal.xyz), ambientLighting);
+    float brightness = max(dot(lightDirection.xyz, worldNormal.xyz), ambientLighting);
     vertexColor = color * brightness;
 }
